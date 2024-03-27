61c450829477e51fcab9669c25321295b7c5816a
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-03-27 18:55:16 +0200

    Iteration 5

--- ironbeers-backend/routes/beers.routes.js
+++ ironbeers-backend/routes/beers.routes.js
@@ -7,6 +7,22 @@ router.get("/beers", (req, res) => {
     console.log(allBeers)
     res.json(allBeers)
   })
+  .catch((err)=> {
+    console.log(err)
+    res.json(err)
+  })
 });
+
+router.get('/beers/:id', (req, res)=> {
+  Beer.findById(req.params.id)
+  .then((beer)=> {
+    console.log(beer)
+    res.json(beer)
+  })
+  .catch((err)=> {
+    res.json(err)
+  })
+})
+
 
 module.exports = router;
