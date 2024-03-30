f2cf16b372cf897cdd9b2518d42cb2d962350401
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-03-30 18:02:11 +0200

    Iteration 13

--- ironbeers-frontend/src/components/Navbar.jsx
+++ ironbeers-frontend/src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import home_icon from "../assets/home-icon.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <header>      
+      <nav style={{ backgroundColor: "blue" }}>
+        <Link to={"/"}>
+          <img src={home_icon} alt="home icon img" />
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- ironbeers-frontend/src/pages/HomePage.jsx
+++ ironbeers-frontend/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import beers from "../../../assets/beers.png";
-import random_beer from '../../../assets/random-beer.png'
-import new_beer from '../../../assets/new-beer.png'
+import beers from "../assets/beers.png";
+import random_beer from "../assets/random-beer.png";
+import new_beer from "../assets/new-beer.png";
 
 function HomePage() {
   return (
@@ -23,7 +23,7 @@ function HomePage() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac diam eu lectus feugiat lacinia. Vivamus eu ex sed mi pretium porttitor quis nec quam. Aliquam tempor orci in volutpat tempor. Nunc pellentesque neque id gravida ultrices. Pellentesque ut mauris non dolor auctor finibus in id
         tortor. Nam feugiat, metus ut rhoncus accumsan, metus sem mollis sapien, vel sodales ex eros sit amet enim.
       </p>
-      <Link to={'/new-beer'}>
+      <Link to={"/new-beer"}>
         <img src={new_beer} alt="new beer img" />
         <h1>New Beer</h1>
       </Link>
